fa7189c04e947765e8d21b4247140530370ff2ae
Emon <emon.mhk69@gmail.com>
2023-08-26 13:02:09 +0600

    mongodb is connected

--- index.js
+++ index.js
@@ -9,11 +9,40 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get("/", (req,res)=>{
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zyyhzcl.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get("/", (req, res) => {
     res.send("car hub server is running...");
 })
 
 
-app.listen(port, (req,res)=>{
+app.listen(port, (req, res) => {
     console.log(`car-hub-server API is running on port: ${port}`);
 })
