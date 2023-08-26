453bf51b5b60b7d08c613930cd199590d68f0a9b
Emon <emon.mhk69@gmail.com>
2023-08-26 15:20:04 +0600

    deployment done for initial part

--- index.js
+++ index.js
@@ -93,8 +93,8 @@ async function run() {
 
 
 
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     } finally {
         // await client.close();
