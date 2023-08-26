e25f905f9b572d19536f22f7b52f5e9d48463741
Emon <emon.mhk69@gmail.com>
2023-08-26 14:35:33 +0600

    all get query done

--- index.js
+++ index.js
@@ -25,27 +25,51 @@ async function run() {
     try {
         // await client.connect();
 
+        // ----------- all collections ------------
         const carsCollections = client.db("carHub").collection("cars");
         const categorysCollections = client.db("carHub").collection("categorys");
 
-        // get all cars:
-        app.get("/cars", async(req,res)=>{
+        // get all cars with soritng based on price:
+        app.get("/cars", async (req, res) => {
+
             const cars = await carsCollections.find().toArray();
+
+            if (req.query?.sort === 'desc') {
+                cars.sort((a, b) => b.price - a.price); // Sort in descending order based on price
+            } else {
+                cars.sort((a, b) => a.price - b.price); // Sort in ascending order based on price
+            }
             res.send(cars);
         })
 
+
         // get single cars:
-        app.get("/cars/:id", async(req,res)=>{
+        app.get("/cars/:id", async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await carsCollections.findOne(query);
             res.send(result);
         })
 
+
+        // get Top Cars by ratings:
+        app.get("/topCars", async (req, res) => {
+            const cars = await carsCollections.find().sort({ rating: - 1 }).limit(3).toArray();
+            res.send(cars);
+        })
+
+        // get all cars by seller Email:
+        app.get("/getCarsBySellerEmail", async(req, res)=>{
+
+            const query = {sellerEmail : req.query.sellerEmail};
+            const result = await carsCollections.find(query).toArray();
+            res.send(result);
+        })
+
         // get all cars by category Id:
-        app.get("/getCarsByCategory/:id", async(req,res)=>{
+        app.get("/getCarsByCategory/:id", async (req, res) => {
             const id = req.params.id;
-            const query =  {category_id: id};
+            const query = { category_id: id };
             const result = await carsCollections.find(query).toArray();
             res.send(result);
         })
@@ -54,25 +78,24 @@ async function run() {
         // -------------------category api------------------
 
         // get all category:
-        app.get("/categorys", async(req,res)=>{
+        app.get("/categorys", async (req, res) => {
             const categorys = await categorysCollections.find().toArray();
             res.send(categorys)
         })
 
         // get single category:
-        app.get("/categorys/:id", async(req,res)=>{
+        app.get("/categorys/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {category_id: id};
+            const query = { category_id: id };
             const result = await categorysCollections.findOne(query);
             res.send(result);
         })
-        
-
 
 
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
     } finally {
         // await client.close();
     }
