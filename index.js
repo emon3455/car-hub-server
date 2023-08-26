aa2d657e0a34ce1033400d0e4d9b1007a560a5cd
Emon <emon.mhk69@gmail.com>
2023-08-26 13:13:21 +0600

    all category and all cars data display done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -9,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zyyhzcl.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,15 +23,28 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
 
+        const carsCollections = client.db("carHub").collection("cars");
+        const categorysCollections = client.db("carHub").collection("categorys");
 
+        // get all cars
+        app.get("/cars", async(req,res)=>{
+            const cars = await carsCollections.find().toArray();
+            res.send(cars);
+        })
+
+        // get all category
+        app.get("/categorys", async(req,res)=>{
+            const categorys = await categorysCollections.find().toArray();
+            res.send(categorys)
+        })
 
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
